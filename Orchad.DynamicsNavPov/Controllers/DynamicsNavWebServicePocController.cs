a93fa06645afc770a4b8e25e36c633e240b740bb
Tomáš Krchňák <thomas.krchnak@gmail.com>
2018-09-28 00:48:22 -0700

    Modul pre Orchard CMS - testovanie Dynamics NAV webservisov - SOAP & OData, INFO: Dll neexistuje - v pripade problemu s chybajucou DLL (ktora vsak existuje v custom module) je potrebne pridat dany nuget package aj do Orchard.Web projektu  - Pr. pri testovani Dynamics NAV Odata webservisu bolo potrebne do modulu pridat nuget pre "Microsoft.Data.Edm", pri debugovany sa vyskytla exception s info, ze tato DLL chyba, po pridany nugetu aj do projektu "Orchard.Web" bol problem odstraneny Pozri: http://www.szmyd.com.pl/blog/referencing-custom-assemblies-in-orchard-module#.W63VjWgzZaQ

--- Orchad.DynamicsNavPov/Controllers/DynamicsNavWebServicePocController.cs
+++ Orchad.DynamicsNavPov/Controllers/DynamicsNavWebServicePocController.cs
@@ -2,9 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using odata = NAV;
 
 namespace Orchad.DynamicsNavPov.Controllers
 {
@@ -12,6 +14,61 @@ namespace Orchad.DynamicsNavPov.Controllers
     {
         // GET: DynamicsNavWebServicePoc
         public string Index()
+        {
+            string customerInfo = "PoC";            
+            customerInfo = TestDynamicsNavSoapService(); 
+            return customerInfo;
+        }
+
+        public string IndexOdata()
+        {
+            string customerInfo = "PoC";
+            customerInfo = TestDynamicsNavODataWebService();
+            return customerInfo;
+        }
+
+        private static string TestDynamicsNavODataWebService()
+        {
+            var nav = new odata.NAV(new Uri("http://win-k4iu0ll836s:7048/DynamicsNAV90/OData/Company('CRONUS%20International%20Ltd.')/"));
+            nav.Credentials = CredentialCache.DefaultNetworkCredentials;
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Printing list of customers OData");
+            sb.AppendLine(PrintCustomersOdata(nav));
+
+            odata.Customer newCustomer = new odata.Customer();
+            newCustomer.Name = "Customer Name Odata";
+            nav.AddToCustomer(newCustomer);
+            nav.SaveChanges();
+
+            sb.AppendLine(PrintCustomersOdata(nav));
+            return sb.ToString();
+        }
+
+        private static string PrintCustomersOdata(odata.NAV nav)
+        {
+            var sb = new StringBuilder();
+            var customers = (from c in nav.Customer
+                             where c.Name.Contains("a")
+                             select c).ToList();
+            bool customerFound = false;
+            foreach (odata.Customer customer in customers)
+            {
+                customerFound = true;
+                sb.AppendLine($"No.: {customer.No} Name: {customer.Name}");
+            }
+
+            if (!customerFound)
+            {
+                sb.AppendLine("The are no customers fulfilling filter criteria");
+            }
+
+            return sb.ToString();
+        }
+
+
+        private static string  TestDynamicsNavSoapService()
         {
             // Creates instance of service and sets credentials.  
             Customer_Service service = new Customer_Service();
@@ -30,7 +87,7 @@ namespace Orchad.DynamicsNavPov.Controllers
         private static string PrintCustomerList(Customer_Service service, List<Customer_Filter> filter)
         {
             var sb = new StringBuilder();
-            sb.AppendLine("Printing Customer List");
+            sb.AppendLine("Printing Customer List Soap");
 
             // Runs the actual search.  
             Customer[] list = service.ReadMultiple(filter.ToArray(), null, 100);
